03eaf676ee5dd5c752a47fe1b4a953d4c5822cca
michael <michael.lcw99@gmail.com>
2021-06-11 16:22:31 +0800

    Improved UI and added additional functions

--- script.js
+++ script.js
@@ -21,105 +21,109 @@ function operate(a, operator, b) {
 const btnToOperator = {
     "+": add,
     "-": subtract,
-    "*": multiply,
+    "x": multiply,
     "÷": divide,
 }
 
 const displayHTML = document.querySelector("span");
-let memory = []; // stores all the recorded numbers & operators
-let operatorBefore = false; // flag for checking
+let memory = []; 
+let operatorBefore = false; 
+let equalsBefore = false; 
 const zeroDivisionMessage = "you sure?";
 
 const btns = Array.from(document.querySelectorAll("button"));
-const displayableBtns = btns.filter(btn => /[0-9]/.test(btn.textContent));
-const operatorBtns = btns.filter(btn => /[^Clear=0-9]/.test(btn.textContent)); 
+const displayableBtns = btns.filter(btn => /[0-9.]/.test(btn.textContent));
+const operatorBtns = btns.filter(btn => /[^ClearDelete=0-9.]/.test(btn.textContent)); 
 const equalBtn = btns.find(btn => btn.textContent === "=");
 const clearBtn = btns.find(btn => btn.textContent === "Clear");
+const deleteBtn = btns.find(btn => btn.textContent === "Delete");
 
 function displaySelectedBtn() { 
+    if (equalsBefore) {
+        equalsBefore = false;
+        displayHTML.textContent = "";
+    }
+
     if (operatorBefore || displayHTML.textContent === zeroDivisionMessage) { 
         operatorBefore = false;
         displayHTML.textContent = "";
     } 
-    displayHTML.textContent += this.textContent;
+
+    if (displayHTML.textContent.length === 9) {
+        return;
+    }
+
+    if (!/[.]/.test(displayHTML.textContent) || this.textContent !== ".") {
+        displayHTML.textContent += this.textContent;
+    }
 }
 
 displayableBtns.forEach(dispBtn => dispBtn.addEventListener("click", displaySelectedBtn));
 
 operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener("click", function() {
-    // get what is present on the display & stored in memory
-    const num = parseInt(displayHTML.textContent);
-
-    // only if input is a valid number, and we dont want to add the number if before is alrdy operator: + + 
+    const num = Number(displayHTML.textContent);
+    
+    // check validity of current display to be added
     if (!Number.isNaN(num) && !operatorBefore) {
         memory.push(num);
     }
     
-    // For intermediate operations; check if it is already a pair of numbers with operator present
-    // actually.. can we use the same function in equal to evaluate?? (before adding the number);
+    // to proceed with intermediate operations where memory = [num, operator, num]
     if (memory.length === 3) {
-        console.log("operated");
-        console.log(memory);
         const result = operate(...memory);
-        // show result on display
         displayHTML.textContent = result;
-        // reset memory
         memory.splice(0, memory.length); 
         memory.push(result);
     }
-    // retrieve what operator it is
 
+    // check validity of potential new input for adding operator next
     const afterNum = parseInt(displayHTML.textContent);
-
-    if (!operatorBefore) {
+    if (!Number.isNaN(afterNum) && !operatorBefore) {
         const btnType = this.textContent;
         let operator = btnToOperator[`${btnType}`];
         memory.push(operator);
-        // change flag; note we want to check if we did a + +, so we leave changing the flag at the end
-        operatorBefore = true; 
     }
+    operatorBefore = true;
     console.log(memory);
 }));
 
-equalBtn.addEventListener("click", function() {
-    // only if there is [number, operator] in memory, then calculate; for = bugs
-    if (displayHTML.textContent === zeroDivisionMessage) {
-            
-    } else if (memory.length === 2) {
-        const finalNum = parseInt(displayHTML.textContent);
-        const error = checkDivisionByZero(finalNum);
-        if (!error) {
-            memory.push(finalNum);
-            const result = operate(...memory);
-            // show result on display
-            displayHTML.textContent = result;
-            // reset memory
-            memory.splice(0, memory.length); 
-        }
-    }
-});
-
-function calculate() {
-    const result = operate(...memory);
-    // show result on display
-    displayHTML.textContent = result;
-    // reset memory
-    memory.splice(0, memory.length); 
-    return result;
-}
-
-clearBtn.addEventListener("click", function() {
-    displayHTML.textContent = "";
-    memory.splice(0, memory.length); // reset memory
-});
-
+// checks zero division error and changes display to error if so
 function checkDivisionByZero(finalNum) {
     if (finalNum === 0 && memory[memory.length - 1] === divide) {
         displayHTML.textContent = zeroDivisionMessage;
-        memory.splice(0, memory.length); // reset memory
+        memory.splice(0, memory.length);
         return true;
     }
     return false;
 }
 
-// clean up code for the buggy + + scenarios
+equalBtn.addEventListener("click", function() {
+    // only if there is [number, operator] in memory, then calculate; for = bugs after evaluating alrdy
+    if ((memory.length === 2) && (displayHTML.textContent !== zeroDivisionMessage)) {
+        const finalNum = Number(displayHTML.textContent);
+        const error = checkDivisionByZero(finalNum);
+        if (!error) {
+            memory.push(finalNum);
+            const result = operate(...memory);
+            displayHTML.textContent = result;
+            memory.splice(0, memory.length); 
+            equalsBefore = true;
+        }
+    }
+});
+
+clearBtn.addEventListener("click", function() {
+    displayHTML.textContent = "";
+    memory.splice(0, memory.length); 
+});
+
+deleteBtn.addEventListener("click", function() {
+    displayHTML.textContent = displayHTML.textContent.slice(0, -1);
+});
+
+// bug: when doing 9 + *, its stored as 9 + 
+
+// clean up code for displayButton and operatorButton?
+// keyboard support
+// work on animations for buttons
+
